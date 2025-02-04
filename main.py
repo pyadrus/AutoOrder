aeea1c03cf136541694dc02050e975d516a4ab0f
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-02-04 10:45:10 +0300

    Добавлена доплата за работу в военное время.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from supplement_for_achievements_in_work import supplement_for_achievements_in_work
+from supplement_for_work_wartime import supplement_for_work_wartime
 from supplement_minimum_wage import supplement_minimum_wage
 
 # Заполнение приказов
 
 supplement_for_achievements_in_work()  # Доплата за высокие достижения в работе
 supplement_minimum_wage()  # Доплата до МРОТ
+supplement_for_work_wartime()  # Доплата за работу в военное время
---
+++ supplement_for_work_wartime.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+from docxtpl import DocxTemplate
+from loguru import logger
+from openpyxl import load_workbook
+
+
+def supplement_for_work_wartime():
+    """Доплата за работу в военное время"""
+
+    file_dog = "data/sample/Доплата_за_работу_в_военное_время.docx"
+    data_mounts = "январь"
+
+    def record_data_salary_downtime_week():
+        """Заполнение приказа"""
+        doc = DocxTemplate(file_dog)
+
+        # Получаем данные из базы данных
+        rows = get_data_from_db()
+        table_data = prepare_table_data(rows)
+
+        context = {
+            "data_mounts": f" {data_mounts} ",
+            "table_data": table_data  # Передаем данные для таблицы
+        }
+
+        doc.render(context)
+        doc.save(f"data/Доплата_за_работу_в_военное_время_{data_mounts}.docx")
+
+    def opening_the_database():
+        """Открытие базы данных"""
+        conn = sqlite3.connect('data/data.db')  # Создаем соединение с базой данных
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def get_data_from_db():
+        """Извлечение данных из базы данных"""
+        conn, cursor = opening_the_database()
+        cursor.execute('SELECT * FROM data')
+        rows = cursor.fetchall()  # Получаем все строки из таблицы
+        conn.close()
+        return rows
+
+    def prepare_table_data(rows):
+        """Подготовка данных для таблицы"""
+        table_data = []
+        for row in rows:
+            table_data.append({
+                "table_number": row[0],  # Табельный номер
+                "surname_name_patronymic": row[1],  # ФИО
+                "profession": row[2],  # Профессия
+                "percent": row[3]  # Процент
+            })
+        return table_data
+
+    def property_parsing():
+        """Парсинг данных"""
+        try:
+            conn, cursor = opening_the_database()
+            # Открываем выбор файла Excel для чтения данных
+            workbook = load_workbook(filename='data/164.xlsx')  # Загружаем выбранный файл Excel
+            sheet = workbook.active
+
+            # Создаем таблицу в базе данных, если она еще не существует
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS data (table_number, surname_name_patronymic, profession, percent)')
+
+            cursor.execute('DELETE FROM data')
+            conn.commit()  # сохранить изменения
+
+            # Считываем данные из колонок и вставляем их в базу данных
+            for row in sheet.iter_rows(min_row=3, max_row=84, values_only=True):
+                try:
+                    # Проверяем, что строка содержит достаточно данных
+                    if len(row) > 11:  # Убедимся, что в строке есть хотя бы 12 столбцов
+                        table_number = row[1]  # table_number - табельный номер,
+                        surname_name_patronymic = row[2]  # surname_name_patronymic - фамилия,
+                        profession = row[5]  # profession - профессия,
+                        percent = row[11]  # percent - процент
+
+                        # Логируем данные для отладки
+                        logger.info(
+                            f'Данные для вставки: {table_number}, {surname_name_patronymic}, {profession}, {percent}')
+
+                        # Вставляем данные в таблицу
+                        cursor.execute('INSERT INTO data VALUES (?, ?, ?, ?)',
+                                       (table_number, surname_name_patronymic, profession, percent))
+                    else:
+                        logger.warning(f"Строка содержит недостаточно данных: {row}")
+                except Exception as e:
+                    logger.error(f"Ошибка при обработке строки {row}: {e}")
+
+            # Сохраняем изменения в базе данных и закрываем соединение
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            logger.exception(e)
+
+    property_parsing()
+    record_data_salary_downtime_week()
+
+
+if __name__ == '__main__':
+    supplement_for_work_wartime()
