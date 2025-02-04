f194f874ccea3aeac8a7b9678cbbf12c1ec9b17e
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-02-04 10:08:07 +0300

    Добавлена доплата до МРОТ

---
+++ supplement_minimum_wage.py
@@ -0,0 +1,100 @@
+import sqlite3
+from docxtpl import DocxTemplate
+from openpyxl import load_workbook
+from loguru import logger
+
+file_dog = "data/Доплата_до_МРОТ.docx"
+data_mounts = "январь"
+
+
+def record_data_salary_downtime_week():
+    """Заполнение приказа"""
+    doc = DocxTemplate(file_dog)
+
+    # Получаем данные из базы данных
+    rows = get_data_from_db()
+    table_data = prepare_table_data(rows)
+
+    context = {
+        "data_mounts": f" {data_mounts} ",
+        "table_data": table_data  # Передаем данные для таблицы
+    }
+
+    doc.render(context)
+    doc.save(f"data/Доплата_до_МРОТ_{data_mounts}.docx")
+
+
+def opening_the_database():
+    """Открытие базы данных"""
+    conn = sqlite3.connect('data/data.db')  # Создаем соединение с базой данных
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def get_data_from_db():
+    """Извлечение данных из базы данных"""
+    conn, cursor = opening_the_database()
+    cursor.execute('SELECT * FROM data')
+    rows = cursor.fetchall()  # Получаем все строки из таблицы
+    conn.close()
+    return rows
+
+
+def prepare_table_data(rows):
+    """Подготовка данных для таблицы"""
+    table_data = []
+    for row in rows:
+        table_data.append({
+            "table_number": row[0],  # Табельный номер
+            "surname_name_patronymic": row[1],  # ФИО
+            "profession": row[2],  # Профессия
+            "percent": row[3]  # Процент
+        })
+    return table_data
+
+
+def property_parsing():
+    """Парсинг данных"""
+    try:
+        conn, cursor = opening_the_database()
+        # Открываем выбор файла Excel для чтения данных
+        workbook = load_workbook(filename='data/130.xlsx')  # Загружаем выбранный файл Excel
+        sheet = workbook.active
+
+        # Создаем таблицу в базе данных, если она еще не существует
+        cursor.execute('CREATE TABLE IF NOT EXISTS data (table_number, surname_name_patronymic, profession, percent)')
+
+        cursor.execute('DELETE FROM data')
+        conn.commit()  # сохранить изменения
+
+        # Считываем данные из колонок и вставляем их в базу данных
+        for row in sheet.iter_rows(min_row=6, max_row=44, values_only=True):
+            try:
+                # Проверяем, что строка содержит достаточно данных
+                if len(row) > 8:  # Убедимся, что в строке есть хотя бы 12 столбцов
+                    # Счет колонок начинается с 0
+                    table_number = row[0]  # table_number - табельный номер,
+                    surname_name_patronymic = row[1]  # surname_name_patronymic - фамилия,
+                    profession = row[3]  # profession - профессия,
+                    percent = row[5]  # percent - сумма выплаты
+
+                    # Логируем данные для отладки
+                    logger.info(f'Данные для вставки: {table_number}, {surname_name_patronymic}, {profession}, {percent}')
+
+                    # Вставляем данные в таблицу
+                    cursor.execute('INSERT INTO data VALUES (?, ?, ?, ?)',
+                                   (table_number, surname_name_patronymic, profession, percent))
+                else:
+                    logger.warning(f"Строка содержит недостаточно данных: {row}")
+            except Exception as e:
+                logger.error(f"Ошибка при обработке строки {row}: {e}")
+
+        # Сохраняем изменения в базе данных и закрываем соединение
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        logger.exception(e)
+
+
+property_parsing()
+record_data_salary_downtime_week()
